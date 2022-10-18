4b70581e8e582771f4ffad3f6e63e457e2de4ef8
estidlore <estidlore@outlook.com>
2022-10-18 13:14:38 -0500

    setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# IoT Azure Raspberry Pi
+---
+This is a wiki for the IoT Azure Raspberry Pi automation's workshop
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react";
+import React from "react";
+
+const App: FC = (): JSX.Element => (
+  <div>{"iot-azure-raspberry-pi"}</div>
+);
+
+export { App };
